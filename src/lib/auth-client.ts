a31c4835774f33be56750a2deaa8c64ae4eaabf5
Urvil Patel <uvpatel7271@gmail.com>
2026-07-22 15:59:00 +0530

    fix auth url

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -4,7 +4,6 @@ import { createAuthClient } from "better-auth/react"
 
 export const authClient = createAuthClient({
   baseURL:
-    process.env.NEXT_PUBLIC_APP_URL ??
-    "http://localhost:3000",
+    process.env.NEXT_PUBLIC_APP_URL!
     
 })
